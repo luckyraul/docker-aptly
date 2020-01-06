38fe18064eccc383a7e4ddc8fbbcd679d07cff94
Nikita <luckyraul@users.noreply.github.com>
2020-01-06 20:09:31 +0300

    gpg folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ export HOME=/var/lib/aptly
 
 getent group aptly > /dev/null || groupadd --system -g $GROUP_ID aptly
 getent passwd aptly > /dev/null || useradd --system --shell /bin/bash -u $USER_ID -g aptly -d ${HOME} -m aptly 1>/dev/null 2>/dev/null
+mkdir -p /var/lib/aptly/.gnupg
+chown -R aptly:aptly /var/lib/aptly/.gnupg
 
 if [ $(stat -c '%u' ${HOME}) != $USER_ID ]; then
     chown -R aptly:aptly ${HOME}
