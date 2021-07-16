0c2efabada308e252f68e4b25c173099e8165bc2
Nikita <luckyraul@users.noreply.github.com>
2021-07-16 19:33:58 +0300

    aptly chmod

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ getent group aptly > /dev/null || groupadd --system -g $GROUP_ID aptly
 getent passwd aptly > /dev/null || useradd --system --shell /bin/bash -u $USER_ID -g aptly -d ${HOME} -m aptly 1>/dev/null 2>/dev/null
 mkdir -p /var/lib/aptly/.gnupg
 chown -R aptly:aptly /var/lib/aptly/.gnupg
+chmod 700 /var/lib/aptly/.gnupg
 
 if [ $(stat -c '%u' ${HOME}) != $USER_ID ]; then
     chown -R aptly:aptly ${HOME}
